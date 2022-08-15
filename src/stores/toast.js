4eeaa7d20aa5cf76fab204d9410035600b23032c
haumq <haumq@uef.edu.vn>
2022-08-15 16:31:34 +0700

    update toast

---
+++ src/stores/toast.js
@@ -0,0 +1,51 @@
+import { defineStore } from 'pinia'
+
+export const useToastStore = defineStore('toast', {
+  state: () => ({
+    show: false,
+    toastList: [],
+    defaults :{
+      title: "undefined title",
+      body: "undefined body",
+      timeout: 5,
+    },
+
+  }),
+
+  actions: {
+    add (params) {
+      for (let key in this.defaults) {
+        if (params[key] === undefined) {
+          params[key] = this.defaults[key];
+        }
+      }
+
+      params.created = Date.now();
+      params.id = Math.random();
+      params.expire = setTimeout(() => {
+          this.remove(params.id);
+      }, params.timeout * 1000);
+      this.toastList.push(params)
+    },
+    remove (id) {
+      this.toastList.splice(this.index(id), 1);
+    },
+    index (id) {
+      for (let key in this.toastList) {
+        if (id === this.toastList[key].id) {
+          return key;
+        }
+      }
+    },
+    type (type) {
+      switch (type) {
+        case "error":
+          return "is-danger";
+        case "success":
+          return "is-success";
+        case "info":
+          return "is-info";
+      }
+    }
+  }
+})
